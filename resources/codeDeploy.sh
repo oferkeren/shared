6e54e497b290d388a651ac710fe8e038d18488f9
oferkeren <oferkeren@gmail.com>
2019-07-14 17:21:59 +0300

    Update codeDeploy.sh

--- resources/codeDeploy.sh
+++ resources/codeDeploy.sh
@@ -1,10 +1,11 @@
 export AWS_DEFAULT_REGION=eu-west-1
 echo $AWS_DEFAULT_REGION
-aws deploy list-deployments --deployment-group-name CCMServiceDevelopment --application-name CCMServiceDevelopment --include-only-statuses Created Queued InProgress > deployments.json 
-cat deployments.json 
-DID=$(cat deployments.json | awk -F=":" -v RS="," '$1~/"deployments"/ {print}')
-echo $DID
-SUBSTRDID=$(echo $DID | cut -d'"' -f 4) 
-echo $SUBSTRDID 
-aws deploy wait deployment-successful --deployment-id $SUBSTRDID
-rm deployments.json
+aws deploy list-deployments --deployment-group-name CCMServiceDevelopment --application-name CCMServiceDevelopment --include-only-statuses Created Queued InProgress 
+#> deployments.json 
+#cat deployments.json 
+#DID=$(cat deployments.json | awk -F=":" -v RS="," '$1~/"deployments"/ {print}')
+#echo $DID
+#SUBSTRDID=$(echo $DID | cut -d'"' -f 4) 
+#echo $SUBSTRDID 
+#aws deploy wait deployment-successful --deployment-id $SUBSTRDID
+#rm deployments.json
