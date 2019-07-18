28faa971fb5d2aebec4a73c55a281478b08a5e19
oferkeren <oferkeren@gmail.com>
2019-07-18 13:34:27 +0300

    Update codeDeploy.sh

--- resources/codeDeploy.sh
+++ resources/codeDeploy.sh
@@ -1,8 +1,8 @@
 export AWS_DEFAULT_REGION=eu-west-1
 echo $AWS_DEFAULT_REGION
-aws deploy list-deployments --deployment-group-name CCMServiceDevelopment --application-name CCMServiceDevelopment --include-only-statuses Created Queued InProgress > deployments.json 
-sleep 120
-cat deployments.json 
+aws deploy push --application-name $CODE_DEPLOY_APPLICATION --s3-location s3://$CODE_DEPLOY_S3_BUCKET/CCMService.zip --source $CODE_DEPLOY_SUBDIRECTORY
+aws deploy create-deployment --application-name $CODE_DEPLOY_APPLICATION --deployment-group-name $CODE_DEPLOY_DEPLOYMENT_GROUP --s3-location bucket=$CODE_DEPLOY_S3_BUCKET,key=CCMService.zip,bundleType=zip
+#cat deployments.json 
 #DID=$(cat deployments.json | awk -F=":" -v RS="," '$1~/"deployments"/ {print}')
 #echo $DID
 #SUBSTRDID=$(echo $DID | cut -d'"' -f 4) 
