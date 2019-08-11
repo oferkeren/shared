552cdba9ca44999e4014c0c93f2fe99de6a34f2a
oferkeren <oferkeren@gmail.com>
2019-08-11 11:28:41 +0300

    Update mavenAniAli.sh

--- resources/mavenAniAli.sh
+++ resources/mavenAniAli.sh
@@ -1,1 +1,1 @@
-cd ${WORKSPACE} && mvn -DargLine="-Xms2g -Dconfig.resource=local.conf -Dorg.slf4j.simpleLogger.log.scala.slick=warn" clean package
+cd ${WORKSPACE} && /home/jenkins/apache-maven-3.6.1/bin/mvn -DargLine="-Xms2g -Dconfig.resource=local.conf -Dorg.slf4j.simpleLogger.log.scala.slick=warn" clean package
